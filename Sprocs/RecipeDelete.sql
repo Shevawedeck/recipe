34cb56659bb43509d7bb55beef78ada333259ffa
shevawedeck <shevawedeck@gmail.com>
2024-03-19 12:23:29 -0400

    #48 hw 21 corrections

--- Sprocs/RecipeDelete.sql
+++ Sprocs/RecipeDelete.sql
@@ -6,22 +6,22 @@ as
 begin
     declare @return int = 0
     
-    if exists(select * from MealCourseRecipe m left join CookbookRecipe cr on cr.RecipeId = m.RecipeId where m.RecipeId = @RecipeId or cr.RecipeId = @RecipeId)
-    begin
-        select @return = 1, @Message = 'Cannot return recipe that is part of a meal or cookbook.'
-        goto finished
-    end
+    --if exists(select * from MealCourseRecipe m left join CookbookRecipe cr on cr.RecipeId = m.RecipeId where m.RecipeId = @RecipeId or cr.RecipeId = @RecipeId)
+    --begin
+    --    select @return = 1, @Message = 'Cannot delete recipe that is part of a meal or cookbook.'
+    --    goto finished
+    --end
     
     if exists(select * from Recipe r where r.RecipeId = @RecipeId and (datediff(day, r.DateArchived, current_timestamp) <= 30 or r.RecipeStatus = 'published'))
     begin
-        select @return = 1, @Message = 'Can only return recipe that is archived for more than 30 days or is currently drafted.'
+        select @return = 1, @Message = 'Can only delete recipe that is archived for more than 30 days or is currently drafted.'
         goto finished
     end 
    
     begin try
         begin tran
-        delete CookbookRecipe where RecipeId = @RecipeId
-        delete MealCourseRecipe where RecipeId = @RecipeId
+        --delete CookbookRecipe where RecipeId = @RecipeId
+        --delete MealCourseRecipe where RecipeId = @RecipeId
         delete RecipeIngredient where RecipeId = @RecipeId
         delete Direction where RecipeId = @RecipeId
         delete Recipe where RecipeId = @RecipeId
@@ -37,3 +37,4 @@ begin
 end
 go
 
+--select * from Recipe r join MealCourseRecipe m on m.RecipeId = r.RecipeId join CookbookRecipe cr on cr.RecipeId = r.RecipeId where m.RecipeId = @RecipeId or cr.RecipeId = @RecipeId
